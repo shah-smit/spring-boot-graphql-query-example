8fea6bb9afedfaa4fa96c7d863ef4c24eb6a40fa
Smit <smitshah95@hotmail.com>
2020-06-18 22:39:07 +0800

    added files and changes for config server

--- src/main/java/com/techprimers/graphql/springbootgrapqlexample/resource/BookResource.java
+++ src/main/java/com/techprimers/graphql/springbootgrapqlexample/resource/BookResource.java
@@ -3,12 +3,10 @@ package com.techprimers.graphql.springbootgrapqlexample.resource;
 import com.techprimers.graphql.springbootgrapqlexample.service.GraphQLService;
 import graphql.ExecutionResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/rest/books")
 @RestController
@@ -17,10 +15,18 @@ public class BookResource {
     @Autowired
     GraphQLService graphQLService;
 
+    @Value("${property.message}")
+    private String message;
+
     @PostMapping
     public ResponseEntity<Object> getAllBooks(@RequestBody String query) {
         ExecutionResult execute = graphQLService.getGraphQL().execute(query);
 
         return new ResponseEntity<>(execute, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public String getMessage(){
+        return message;
     }
 }
